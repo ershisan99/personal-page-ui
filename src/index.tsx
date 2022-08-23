800b21c898955dbcaa02f94b80390b9b58d84759
Andres <andres99.dev@gmail.com>
2022-08-23 20:28:52 +0200

    add export for page header

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export * from './components/error404/Error404';
 export * from './components/info-block/InfoBlock';
 export * from './components/modal/Modal';
+export * from './components/page-header/PageHeader';
 export * from './components/pagination/Pagination';
 export * from './theme/theme';
